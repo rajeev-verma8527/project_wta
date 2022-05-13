4c2235af6fb7e260014d0a1b7325a36754485e5f
Rajeev Verma <rajeevverma8527@gmail.com>
2022-05-13 17:21:18 +0530

    init

---
+++ database.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+
+Base = declarative_base()
+
+
+
+class Websites(Base):
+    __tablename__ = "websites"
+    id = Column(Integer, primary_key=True)
+    domain = Column(String(255))
+    created = Column(DateTime, default=datetime.utcnow)
+
+
+class Data(Base):
+    __tablename__ = "data"
+    id = Column(Integer, primary_key=True)
+    
+
+class PageVisits(Base):
+    id = Column(Integer, primary_key=True)
+    uuid = Column(String(32))
+    page = Column(String(255))
+    loadtime = Column(Integer)
+    browser = Column(String(355))
+    device_type = Column(String(255))
+    device_os = Column(String(255))
+    country = Column(String(255))
+    state = Column(String(255))
+    city = Column(String(255))
+
+
